f67ebcd60a30a5f271111fbec36e55267f79902c
Testyy117 <testimonymak@gmail.com>
2026-09-12 21:39:57 +0100

    Fix Firebase import path with four dots

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../lib/Firebase";
+import { auth } from "../../../lib/Firebase";
 
 
 import { 
